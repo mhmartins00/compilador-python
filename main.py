184d82635175eb5a4898594d1ca990a42be844ed
Murillo Martins <martins.murillo00@gmail.com>
2022-10-30 00:47:18 -0300

    Validacao em sequencia das declaraçoes separadas por virgula

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-#  README
-#  Para executar basta estar no mesmo diretório do arquivo main.py e digitar > python main.py.
-# 1) número de telefone VARIAVEL = 00900000000 [obrigatorio o comeco com o 9]
-# 2) placas de carro = xxx0000 | XXX000 [3 letras seguidas por 4 numeros]
-# 3) TIPO = 123.456.789-01 [qualquer numero 11 digitos mas com mascara] 
-# 4) números reais = 123.5555555 [de qualquer tamanho]
-# 5) tags html = <qualquer_coisa INCLUSIVE com espaço = @>
-# 6) URL de páginas = HTTP:// | http:// | HTTPS:// | https:// [qualquer coisa seguida]
-# 7) palavras = aass ou AAAS ou asAD [sem numeros e simbolos mas com acentos]
-# 8) cnpj = 12345678901234 [qualquer numero 14 digitos sem mascara]
-# 9) identificadores ling. C = [ids do C int, float, etc...]
 
 import ply.lex as lex
 import ply.yacc as yacc
 
 tokens = (
-    'TIPO',
+    'DECLARACAO',
     'VARIAVEL',
 )
 
@@ -32,9 +21,9 @@ tokens = (
 t_ignore = ' \t'
 
 # Definicao das funcoes com as expressoes regulares
-def t_TIPO( t ) :
+def t_DECLARACAO( t ) :
     # r'\b(int|float|double|char)\s+(\w+)\s*\([^)]*\)\s*{
-    r'^(int|float|char)\s+([_a-zA-Z])+;'
+    r'^(int|float|char)\s+([_a-zA-Z])+[a-zA-Z0-9]*([,]?\s[_a-zA-Z])*[;]{1}$'
     return t
 
 def t_error( t ):
@@ -43,9 +32,9 @@ def t_error( t ):
 lexer = lex.lex()
 
 # Definição das funcoes para validacao de tokens
-def p_tipo( p ):
-    'expr : TIPO'
-    p[0] = f'TIPO VÁLIDO'
+def p_declaracao( p ):
+    'expr : DECLARACAO'
+    p[0] = f'DECLARACAO VÁLIDA'
 
 # def p_variavel( p ):
 #     'expr : VARIAVEL'
@@ -75,3 +64,4 @@ while escape != "s":
     print("_________________________________________\n")
     escape = input("Parar? [s / n]: ")
     print("_________________________________________\n")
+
